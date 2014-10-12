6d1dace50a1e95d3650d0f4e3aa5dc33c3dd45a7
Anthony Edwards Jr <credential.helper>
2014-10-11 23:22:53 -0400

    changed from address to inquiry@jforu.com

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,8 +1,8 @@
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-:address => "smtp.gmail.com",
-:port => 587,
-:domain => "jforu.com",
+:address => "smtpout.secureserver.net",
+:port => 80,
+:domain => "www.jforu.com",
 :user_name =>  Rails.application.secrets.GO_DADDY_USER_NAME,
 :password => Rails.application.secrets.GO_DADDY_PASSWORD,
 :authentication => "plain",
