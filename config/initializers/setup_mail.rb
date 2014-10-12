0cf823124d39481943c4d4d5bead015d7490e545
Anthony Edwards Jr <credential.helper>
2014-10-11 22:51:12 -0400

    add production email env variables

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,8 +3,8 @@ ActionMailer::Base.smtp_settings = {
 :address => "smtp.gmail.com",
 :port => 587,
 :domain => "jforu.com",
-:user_name => "anthony.edwardsjr@gmail.com",
-:password => "",
+:user_name =>  Rails.application.secrets.GO_DADDY_USER_NAME,
+:password => Rails.application.secrets.GO_DADDY_PASSWORD,
 :authentication => "plain",
 :enable_starttls_auto => true
 }
